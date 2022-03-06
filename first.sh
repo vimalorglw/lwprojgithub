3e561577fa834505da7eedc403ee32b65302b1f3
devparth19 <100505251+devparth19@users.noreply.github.com>
2022-03-06 09:39:56 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -2,3 +2,8 @@ date
 cal
 echo how ru
 hello vimal sir
+
+👋 Hi, I’m @devparth19
+👀 I’m interested in sovling error
+🌱 I’m currently learning LInux,ML,AI,Data SCi
+💞️ I’m looking to collaborate on everyone who need to solve error
